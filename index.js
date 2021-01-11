54e302fa842eb71af56a76d60fd7655f67f187cd
AL Mahdi <virtualmahdi@gmail.com>
2021-01-11 19:53:07 +0600

    pointed the links to the pages added before

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ const server = app.listen(port, function () {
 
 // Static files
 app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
