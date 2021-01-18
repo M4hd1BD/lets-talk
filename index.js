128cea50cb438e2b75bff8eb44827447bbdef991
AL Mahdi <virtualmahdi@gmail.com>
2021-01-18 13:01:58 +0600

    added cover picture option

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { config, engine } = require('express-edge');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Post = require('./database/models/post');
+const fileUpload = require("express-fileupload");
 
 // App setup
 let port = process.env.PORT;
@@ -18,7 +19,7 @@ app.use(express.static('public'));
 
 //set up edge
 app.use(engine);
-
+app.use(fileUpload());
 app.set('views', __dirname + '/views');
 
 app.use(bodyParser.json())
@@ -37,9 +38,18 @@ app.get('/posts/new', (req, res) => {
     res.render('create')
 });
 
-app.post('/posts/store', (req, res) => {
-    Post.create(req.body, (error, post) => {
-        res.redirect('/')
+app.post("/posts/store", (req, res) => {
+    const {
+        image
+    } = req.files
+
+    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+        Post.create({
+            ...req.body,
+            image: `/posts/${image.name}`
+        }, (error, post) => {
+            res.redirect('/');
+        });
     })
 });
 
