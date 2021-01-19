e42c794321fa6a20114445fbb6cbcf151493cf0c
AL Mahdi <virtualmahdi@gmail.com>
2021-01-19 12:24:19 +0600

    distributed the codes in small parts to keep index clean

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Post = require('./database/models/post');
 const fileUpload = require("express-fileupload");
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homePage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
 
 // App setup
 let port = process.env.PORT;
@@ -19,6 +23,7 @@ app.use(express.static('public'));
 
 //set up edge
 app.use(engine);
+
 app.use(fileUpload());
 app.set('views', __dirname + '/views');
 
@@ -30,38 +35,10 @@ app.use(bodyParser.urlencoded({
 const storePost = require('./middleware/storePost')
 app.use('/posts/store', storePost)
 
-app.get('/', async (req, res) => {
-    const posts = await Post.find({})
-    res.render('index', {
-        posts
-    })
-});
-
-app.get('/posts/new', (req, res) => {
-    res.render('create')
-});
-
-app.post("/posts/store", (req, res) => {
-    const {
-        image
-    } = req.files
-
-    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-        Post.create({
-            ...req.body,
-            image: `/posts/${image.name}`
-        }, (error, post) => {
-            res.redirect('/');
-        });
-    })
-});
-
-app.get('/post/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('post', {
-        post
-    })
-});
+app.get("/", homePageController);
+app.get("/posts/new", createPostController);
+app.post("/posts/store", storePostController);
+app.get("/post/:id", getPostController);
 
 const server = app.listen(port, function () {
   console.log(`Listening on port ${port}`);
