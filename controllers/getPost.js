bccbd2aa9c8f0471267cd0afd35841e79be0dbbc
AL Mahdi <virtualmahdi@gmail.com>
2021-01-26 12:08:33 +0600

    fixed a small bug

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,8 +1,15 @@
 const Post = require('../database/models/post')
 
-module.exports = async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    res.render("post", {
-        post
+module.exports = (req, res) => {
+    Post.findById(req.params.id, function(err, post) {
+      if (err) {
+        console.log(err);
+        res.redirect('/')
+      }
+      else {
+        res.render("post", {
+            post
+        });
+      }
     });
 }
