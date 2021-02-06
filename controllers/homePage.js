f48e6fe5ad9e214b70f1e9565c571266ee26b96f
AL Mahdi <virtualmahdi@gmail.com>
2021-02-06 12:09:59 +0600

    a small bug fix

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -4,32 +4,39 @@ module.exports = async (req, res) => {
   const limit = 5;
   const page = parseInt(req.params.page) || 1;
 
-  try {
-    // execute query with page and limit values
-    const posts = await Posts.find( {approved: true} )
-      .limit(limit * 1)
-      .skip((page - 1) * limit)
-      .exec();
+  // get total documents in the Posts collection
+  const count = await Posts.countDocuments( {approved: true} );
+  const total = Math.ceil(count / limit);
 
-    // get total documents in the Posts collection
-    const count = await Posts.countDocuments( {approved: true} );
-    const total = Math.ceil(count / limit);
-    var tpgs = [];
+  if (page > total) {
+    res.redirect("/notfound");
+  }
+  else {
+    try {
+      // execute query with page and limit values
+      const posts = await Posts.find( {approved: true} )
+        .limit(limit * 1)
+        .skip((page - 1) * limit)
+        .exec();
 
-    // edge apparently don't iterate a number (i guess there's nothing to iterate)
-    // that's why making that number into an array, so that I can iterate properly
-    for (let i = 1; i <= total; i++) {
-      tpgs.push(i);
-    }
 
-    // return response with posts, total pages, and current page
-    res.render("index",{
-      psts: posts,
-      totalPages: tpgs,
-      currentPage: page,
-      total: total
-    });
-  } catch (err) {
-    console.error(err.message);
+      var tpgs = [];
+
+      // edge apparently don't iterate a number (i guess there's nothing to iterate)
+      // that's why making that number into an array, so that I can iterate properly
+      for (let i = 1; i <= total; i++) {
+        tpgs.push(i);
+      }
+
+      // return response with posts, total pages, and current page
+      res.render("index",{
+        psts: posts,
+        totalPages: tpgs,
+        currentPage: page,
+        total: total
+      });
+    } catch (err) {
+      console.error(err.message);
+    }  
   }
 }
