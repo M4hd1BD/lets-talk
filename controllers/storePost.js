8122b7ec4b265a0f49f4466bc46df9bf04669071
AL Mahdi <virtualmahdi@gmail.com>
2021-02-07 13:29:03 +0600

    automatic approval for administrator

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -2,6 +2,8 @@ const path = require('path')
 const Post = require('../database/models/post')
 const User = require('../database/models/user')
 let uname = "";
+let aprvd = false;
+
 module.exports = (req, res) => {
     const {
         image
@@ -13,13 +15,17 @@ module.exports = (req, res) => {
       }
       else {
         uname = user.username;
+        if (user.administrator == true) {
+          aprvd = true;
+        }
       }
     })
+
     image.mv(path.resolve(__dirname, '..', 'public/posts', image.name), (error) => {
         Post.create({
             ...req.body,
             image: `/posts/${image.name}`,
-            approved: true,
+            approved: aprvd,
             username: uname
         }, (error, post) => {
             res.redirect("/");
