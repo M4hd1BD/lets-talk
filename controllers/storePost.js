f3fd39c07056e1bd1eddea9df7ad1a8454a35167
AL Mahdi <virtualmahdi@gmail.com>
2021-02-07 13:05:18 +0600

    fixed a small bug

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -1,16 +1,26 @@
 const path = require('path')
 const Post = require('../database/models/post')
-
+const User = require('../database/models/user')
+let uname = "";
 module.exports = (req, res) => {
     const {
         image
     } = req.files
 
+    User.findById(req.session.userId, (error, user) => {
+      if(error || !user){
+        res.redirect('/');
+      }
+      else {
+        uname = user.username;
+      }
+    })
     image.mv(path.resolve(__dirname, '..', 'public/posts', image.name), (error) => {
         Post.create({
             ...req.body,
             image: `/posts/${image.name}`,
-            approved: true
+            approved: true,
+            username: uname
         }, (error, post) => {
             res.redirect("/");
         });
